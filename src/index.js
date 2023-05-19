62e5faf009727028b22554e777a2dc2acea3fa32
Aman Chauhan <amangotpro@gmail.com>
2023-05-19 22:53:15 +0530

    add: auth_context

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { makeServer } from "./server";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { DataContext, ContextData } from "./Contexts/data-context";
+import { AuthProvider, AuthContext } from "./Contexts/auth-context";
 
-export {ContextData}
+export {ContextData, AuthContext}
 
 // Call make Server
 makeServer();
@@ -15,9 +16,11 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <DataContext>
-    <Router>
+      <AuthProvider>
+      <Router>
     <App />
     </Router>
+      </AuthProvider>
     </DataContext>
     
     
