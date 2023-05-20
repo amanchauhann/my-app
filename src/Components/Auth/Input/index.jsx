4fab87ac458fba7af6685e661eda927b88452c56
Aman Chauhan <amangotpro@gmail.com>
2023-05-20 12:32:49 +0530

    removed: auth_input_component

--- src/Components/Auth/Input/index.jsx
+++
@@ -1,9 +0,0 @@
-const Input = ({id, label, type, placeholder}) => {
-    return (
-        <>
-            <label for={id}>{label}</label>
-            <input className="auth-input" type={type} placeholder={placeholder} id={id}/>
-        </>
-    )
-}
-export default Input
