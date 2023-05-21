ffd07fbfa44ae6c42bf0320283c79eb05d261f95
Aman Chauhan <amangotpro@gmail.com>
2023-05-22 02:28:38 +0530

    add: home_category

--- src/Components/Home-Components/Categories/CategoryCard/index.jsx
+++ src/Components/Home-Components/Categories/CategoryCard/index.jsx
@@ -1,7 +1,17 @@
+import { useConst } from "@chakra-ui/hooks";
 import "./CategoryCard.css";
+import { useContext } from "react";
+import { ContextData } from "../../../../index";
+import { useNavigate } from "react-router";
 
 const CategoryCard = ({ categoryImage, categoryName, description }) => {
-
+    const { categoryHandler, resetFilters } = useContext(ContextData)
+    const navigate = useNavigate()
+    const home_category_handler = () => {
+        resetFilters()
+        categoryHandler(categoryName)
+        navigate("/products")
+    }
     return (
         <>
             <div className="category-card">
@@ -9,7 +19,7 @@ const CategoryCard = ({ categoryImage, categoryName, description }) => {
                 <div className="text-container">
                     <h2 className="category-headline">{categoryName}</h2>
                     <p className="category-description">{description}</p>
-                    <a href="#" className="category-explore">Explore more  {">"}</a>
+                    <div onClick={home_category_handler} className="category-explore">Explore more  {">"}</div>
                 </div>
 
             </div>
--- src/Contexts/data-context.js
+++ src/Contexts/data-context.js
@@ -3,24 +3,24 @@ import { useParams } from "react-router-dom";
 
 export const ContextData = createContext()
 
-export const DataContext = ({children}) => {
+export const DataContext = ({ children }) => {
     const [categoriesData, setCategoriesData] = useState([])
-    const [productsData,setProductsData] = useState([])
+    const [productsData, setProductsData] = useState([])
     // const [filteredPrice, setFilteredPrice] = useState(0)
     const [userFilters, setUserFilters] = useState({
         price: 0,
         categories: [],
-        weight: {min: "", max: Infinity},
+        weight: { min: "", max: Infinity },
         ratings: null,
         sort: ""
     })
     const [selectedProduct, setSelectedProduct] = useState([])
-    const {ProductID} = useParams()
+    const { ProductID } = useParams()
     // console.log(ProductID)
 
-    useEffect(()=>{
-        const fetchingData = async () =>{
-            try{
+    useEffect(() => {
+        const fetchingData = async () => {
+            try {
                 const getCategoriesRes = await fetch("/api/categories")
                 const getCategoriesData = await getCategoriesRes.json()
                 const getCategories = await getCategoriesData.categories
@@ -29,31 +29,31 @@ export const DataContext = ({children}) => {
                 const res = await fetch("/api/products")
                 const data = await res.json()
                 setProductsData(data.products)
-            }catch(e){
+            } catch (e) {
                 console.log(e.target.value)
             }
         }
         fetchingData()
-    },[])
+    }, [])
 
     const priceHandler = (e) => {
-        setUserFilters(prev=> ({...prev, price: e.target.value}))
+        setUserFilters(prev => ({ ...prev, price: e.target.value }))
         // console.log(e.target.name)
     }
 
-    const categoryHandler = (e) =>{
-        console.log("aaa", e.target.value)
-        setUserFilters(prev=> {
-            return (prev.categories).find(existingCategory => existingCategory === e.target.value) ? {...prev, categories: (prev.categories).filter(eachPrevExistingCategory => eachPrevExistingCategory !== e.target.value)} : {...prev, categories: [...prev.categories, e.target.value]}
+    const categoryHandler = (filter_category) => {
+        // console.log("aaa", e.target.value)
+        setUserFilters(prev => {
+            return (prev.categories).find(existingCategory => existingCategory === filter_category) ? { ...prev, categories: (prev.categories).filter(eachPrevExistingCategory => eachPrevExistingCategory !== filter_category) } : { ...prev, categories: [...prev.categories, filter_category] }
         })
     }
 
     const weightHandler = (e) => {
-        setUserFilters(prev => ({...prev, weight: {min: e.target.min, max: e.target.max}}))
+        setUserFilters(prev => ({ ...prev, weight: { min: e.target.min, max: e.target.max } }))
     }
 
     const ratingHandler = (e) => {
-        setUserFilters(prev => ({...prev, ratings: e.target.value}))
+        setUserFilters(prev => ({ ...prev, ratings: e.target.value }))
     }
 
     const resetFilters = () => {
@@ -61,18 +61,18 @@ export const DataContext = ({children}) => {
         setUserFilters({
             price: 0,
             categories: [],
-            weight: {min: 0, max: Infinity},
+            weight: { min: 0, max: Infinity },
             ratings: 0,
             sort: ""
         })
     }
 
     const sortHandler = (e) => {
-        setUserFilters(prev => ({...prev, sort: e.target.value}))
+        setUserFilters(prev => ({ ...prev, sort: e.target.value }))
     }
 
-    return(
-        <ContextData.Provider value={{categoriesData, productsData, priceHandler, categoryHandler, weightHandler, ratingHandler, sortHandler, resetFilters, userFilters}}>
+    return (
+        <ContextData.Provider value={{ categoriesData, productsData, priceHandler, categoryHandler, weightHandler, ratingHandler, sortHandler, resetFilters, userFilters }}>
             {children}
         </ContextData.Provider>
     )
