af1bce2f7e651df1d6d5985d6746646bf605852a
Aman Chauhan <amangotpro@gmail.com>
2023-05-22 00:08:45 +0530

    fix: changed_category_prop

--- src/Components/Home-Components/Categories/CategoryCard/index.jsx
+++ src/Components/Home-Components/Categories/CategoryCard/index.jsx
@@ -1,18 +1,18 @@
 import "./CategoryCard.css";
 
-const CategoryCard = ({categoryImage, headline, description}) => {
-    
-    return(
+const CategoryCard = ({ categoryImage, categoryName, description }) => {
+
+    return (
         <>
-        <div className="category-card">
-            <div className="image-container" style={{ backgroundImage: `url(${categoryImage})` }}></div>
-            <div className="text-container">
-            <h2 className="category-headline">{headline}</h2>
-            <p className="category-description">{description}</p>
-            <a href="#" className="category-explore">Explore more  {">"}</a>
+            <div className="category-card">
+                <div className="image-container" style={{ backgroundImage: `url(${categoryImage})` }}></div>
+                <div className="text-container">
+                    <h2 className="category-headline">{categoryName}</h2>
+                    <p className="category-description">{description}</p>
+                    <a href="#" className="category-explore">Explore more  {">"}</a>
+                </div>
+
             </div>
-            
-        </div>
         </>
     )
 }
