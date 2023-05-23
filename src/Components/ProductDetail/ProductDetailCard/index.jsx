a3cfd69c8f0f870852f4692bb235bf15eada2daa
Aman Chauhan <amangotpro@gmail.com>
2023-05-23 19:21:34 +0530

    add: wishlist_functionality

--- src/Components/ProductDetail/ProductDetailCard/index.jsx
+++ src/Components/ProductDetail/ProductDetailCard/index.jsx
@@ -1,31 +1,39 @@
 import "./ProductDetailCard.css"
 import mini2 from "../../../assets/Products/Mini/mini2.jpg"
+// import WishlistBtn from "../../Action/Button/WishlistBtn"
+import { AuthContext } from "../../../Contexts/auth-context"
+import { useContext } from "react"
 
-const ProductDetailCard = ({productImage, title, ratings, reviewedBy, price, availability, categoryName, description, weight}) => {
+const ProductDetailCard = ({ productImage, title, ratings, reviewedBy, price, availability, categoryName, description, weight, _id, wishlist_btn_handler }) => {
     const soldout = !availability
+    const existing_id = _id
+    const { logged_user, wishListHandler, removeWishlistHandler } = useContext(AuthContext)
+    const is_Wishlisted = logged_user.wishlist.find(({ _id }) => _id === existing_id)
+    console.log("kl", logged_user.wishlist)
     return (
         <main className="product-detail-main-container">
             <div className="product-detail">
-            <div className="product-detail-image-container">
-            <img className="product-detail-image" src={productImage} />
+                <div className="product-detail-image-container">
+                    <img className="product-detail-image" src={productImage} />
+                </div>
+
+                <div className="product-detail-right-container">
+                    <h1>{title}</h1>
+                    <small className="product-detail-review-container">★{ratings} | {(reviewedBy / 1000).toFixed(2)}k reviews</small>
+                    <h1 className="product-detail-price">${price}</h1>
+                    <hr />
+                    <p><span className="font-wt-bold">Availability :</span> {availability ? "in-stock" : "out of stock"}</p>
+                    <p><span className="font-wt-bold">Category :</span> {categoryName}</p>
+                    <p><span className="font-wt-bold">Description :</span> {description}</p>
+                    <p><span className="font-wt-bold">Weight :</span> {weight}Kg</p>
+                    <div className="product-detail-button-container">
+                        <button disabled={soldout} style={{ opacity: soldout ? "0.3" : "1" }} className="product-detail-button-cart product-detail-button-dimension">Add to Cart</button>
+                        {is_Wishlisted ? <button onClick={() => removeWishlistHandler(_id)} className="product-detail-button-wishlist product-detail-button-dimension">Remove</button> : <button disabled={soldout} style={{ opacity: soldout ? "0.3" : "1" }} className="product-detail-button-wishlist product-detail-button-dimension" onClick={wishlist_btn_handler}>Wishlist</button>}
+                        {/* <WishlistBtn soldout={soldout} /> */}
+                    </div>
+
+                </div>
             </div>
-        
-        <div className="product-detail-right-container">
-            <h1>{title}</h1>
-            <small className="product-detail-review-container">★{ratings} | {(reviewedBy/1000).toFixed(2)}k reviews</small>
-            <h1 className="product-detail-price">${price}</h1>
-            <hr />
-            <p><span className="font-wt-bold">Availability :</span> {availability ? "in-stock" : "out of stock"}</p>
-            <p><span className="font-wt-bold">Category :</span> {categoryName}</p>
-            <p><span className="font-wt-bold">Description :</span> {description}</p>
-            <p><span className="font-wt-bold">Weight :</span> {weight}Kg</p>
-            <div className="product-detail-button-container">
-            <button disabled={soldout}  style={{opacity: soldout ? "0.3" : "1"}} className="product-detail-button-cart product-detail-button-dimension">Add to Cart</button>
-            <button disabled={soldout}  style={{opacity: soldout ? "0.3" : "1"}} className="product-detail-button-wishlist product-detail-button-dimension">Wishlist</button>
-            </div>
-            
-        </div>
-        </div>
         </main>
     )
 }
--- src/Services/index.js
+++ src/Services/index.js
@@ -1,34 +1,34 @@
-export const LoginService = async (email, password)=>{
+export const LoginService = async (email, password) => {
     // return 
     const res = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
-        "Content-Type": "application/json",
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
             email,
             password
         }),
     })
-     //storing status of Response.
-    const ResStatus= res.status
+    //storing status of Response.
+    const ResStatus = res.status
 
     const data = await res.json()
-console.log("a", data)
-    return {data, ResStatus};
-     //if there is error, we are setting it in state.
-     // if(ResStatus !== 200){
-     //     setLoginError({status: ResStatus, message: data.errors[0]})
-     // } 
+    console.log("a", data)
+    return { data, ResStatus };
+    //if there is error, we are setting it in state.
+    // if(ResStatus !== 200){
+    //     setLoginError({status: ResStatus, message: data.errors[0]})
+    // } 
 
-     //if user exist, will get encodedToken
+    //if user exist, will get encodedToken
     //  const encodedToken = await data.encodedToken
 
     //  //store token in local storage, but only is user exist
     //  encodedToken && localStorage.setItem('encodedToken', encodedToken);
 }
 
-export const SignupService = async (email, password, name)=>{
+export const SignupService = async (email, password, name) => {
     const res = await fetch("/api/auth/signup", {
         method: "POST",
         headers: {
@@ -43,5 +43,38 @@ export const SignupService = async (email, password, name)=>{
     const status = await res.status
     const data = await res.json()
 
-    return ({data, status})
+    return ({ data, status })
+}
+
+export const wishlistService = async (auth_token, product) => {
+    const res = await fetch("/api/user/wishlist", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `${auth_token}`
+        },
+        body: JSON.stringify({
+            product
+        })
+    })
+    // console.log("res", res.status)
+    const status = await res.status
+    const data = await res.json()
+    const get_wishlist = await data.wishlist
+    return { get_wishlist, status }
+}
+
+export const wishlistDeleteService = async (auth_token, product_id) => {
+    const res = await fetch(`/api/user/wishlist/${product_id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `${auth_token}`
+        }
+    })
+    // console.log("res", res.status)
+    const status = await res.status
+    const data = await res.json()
+    const get_wishlist_deletion = await data.wishlist
+    return { get_wishlist_deletion, status }
 }
