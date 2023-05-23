cec541906372342c3025a0f125cc085bdeb051a8
Aman Chauhan <amangotpro@gmail.com>
2023-05-23 22:44:42 +0530

    fix: null_handling

--- src/Components/Action/Common/MainProductCard/index.jsx
+++ src/Components/Action/Common/MainProductCard/index.jsx
@@ -8,8 +8,8 @@ import outofstock from "../../../../Logos/Utils/outofstock.png"
 const MainProductCard = ({ productImage, ratings, title, price, availability, _id, wishlist_btn_handler, remove_wishlist_btn_handler, id }) => {
     const existing_id = id
     const { logged_user } = useContext(AuthContext)
-    const is_Wishlisted = logged_user.wishlist.find(({ id }) => id === existing_id)
-    console.log("from productcard>>", logged_user.wishlist)
+    const is_Wishlisted = logged_user?.wishlist?.length > 0 ? logged_user?.wishlist?.find(({ id }) => id === existing_id) : false
+    console.log("from productcard>>", logged_user?.wishlist)
 
     const soldOut = !availability;
     return (
