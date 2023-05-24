e49118a444cfa3240296d79d4e004c324c81dfe6
Aman Chauhan <amangotpro@gmail.com>
2023-05-24 16:43:14 +0530

    fix: link_shop_now

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-text-container">
-            <h2 className="one-line-title">Shop with Rone Store</h2>
-        <p className="one-line-desc">Get 1% Credit Rewards, Free Shipping on Orders Over $149, 14-Day Returns, and other exclusive offers.</p>
-        <div className="store-btn">Shop Now</div>
-            </div> 
+                <h2 className="one-line-title">Shop with Rone Store</h2>
+                <p className="one-line-desc">Get 1% Credit Rewards, Free Shipping on Orders Over $149, 14-Day Returns, and other exclusive offers.</p>
+                <Link to="/products">
+                    <div className="store-btn">Shop Now</div>
+                </Link>
+
+            </div>
             <hr />
-        <p className="made-with">Made while eating 🍓</p>
+            <p className="made-with">Made while eating 🍓</p>
         </div>
     )
 }
