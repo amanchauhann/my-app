4ceeb0748542a85a77b09ccbc8d5ad3d22b05c10
Aman Chauhan <amangotpro@gmail.com>
2023-06-04 21:07:10 +0530

    add: nav_indicators

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -7,13 +7,14 @@ import Home from "../../Pages/Home";
 
 const Nav = () => {
     const location = useLocation();
-    const { locationHandler, auth_token } = useContext(AuthContext);
+    const { locationHandler, auth_token, logged_user, cartData } = useContext(AuthContext);
     const [search_value, setSearch_value] = useState("");
     const [suggestions, setSuggestions] = useState([]);
     const [productsData, setProductsData] = useState([])
     // const { productsData } = useContext(ContextData);
     // const dummysuggestion = [...suggestions]
-
+    const wishlistlength = logged_user.wishlist.length
+    const cartlength = cartData.length
     const fetchingData = async () => {
         try {
             const res = await fetch("/api/products")
@@ -109,15 +110,23 @@ const Nav = () => {
                             </Link>
                         )}
                         <Link to="/cart">
-                            <NavList liText={<svg xmlns="http://www.w3.org/2000/svg" width="30" height="26" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                            </svg>} />
+                            <div className="nav_li_container">
+                                <NavList liText={<svg xmlns="http://www.w3.org/2000/svg" width="30" height="26" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                </svg>} />
+                                {cartlength > 0 && <p className="nav_indicatior">{cartlength}</p>}
+                            </div>
+
                         </Link>
 
                         <Link to="/wishlist">
-                            <NavList liText={<svg xmlns="http://www.w3.org/2000/svg" width="30" height="26" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                            </svg>} />
+                            <div className="nav_li_container">
+                                <NavList liText={<svg xmlns="http://www.w3.org/2000/svg" width="30" height="26" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                                </svg>} />
+                                {wishlistlength > 0 && <p className="nav_indicatior">{wishlistlength}</p>}
+                            </div>
+
                         </Link>
 
                     </ul>
