103823d00b7001c52060872505e89e57952bba47
Aman Chauhan <amangotpro@gmail.com>
2023-06-05 00:03:29 +0530

    style: responsive_listing

--- src/Pages/ListedProducts/index.jsx
+++ src/Pages/ListedProducts/index.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
 import Aside from "../../Components/ListedProducts/Aside";
 import ProductListing from "../../Components/ListedProducts/ProductListing";
 import "./ListedProducts.css";
 
 const ListedProducts = () => {
+    const [showfilters, setShowFilters] = useState(false)
+    useEffect(() => {
+        const handleWindowResize = () => {
+            if (window.innerWidth < 620) {
+                setShowFilters(false);
+            } else {
+                setShowFilters(true);
+            }
+        };
+
+        window.addEventListener("resize", handleWindowResize);
+        handleWindowResize(); // Initial check on component mount
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+
+    const handleToggleFilters = () => {
+        setShowFilters(!showfilters);
+    };
     return (
         <div className="listed-products">
-            <Aside />
+            {window.innerWidth < 620 && (
+                <button className="show_filter_btn" onClick={handleToggleFilters}>
+                    {showfilters ? "Hide filters" : "Show filters"}
+                </button>
+            )}
+            {showfilters && <Aside />}
             <ProductListing />
         </div>
     )
