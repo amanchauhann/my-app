d153515a52b20e5febd115028a5f03c4479fb032
Aman Chauhan <amangotpro@gmail.com>
2023-06-07 17:47:36 +0530

    fixed

--- src/Components/ListedProducts/Aside/FiltersContainer/Price/index.jsx
+++ src/Components/ListedProducts/Aside/FiltersContainer/Price/index.jsx
@@ -3,9 +3,17 @@ import { useContext } from "react"
 import { ContextData } from "../../../../../index"
 
 export const Price = () => {
-    const {priceHandler, userFilters} = useContext(ContextData)
-    const {price} = userFilters
+    const { priceHandler, userFilters } = useContext(ContextData)
+    const { price } = userFilters
     return (
-        <input value={price} className="price-slider" onChange={priceHandler} type="range" min="0" max="10000" name="filter-price" />
+        <>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p>0</p><p>10,000</p>
+            </div>
+
+            <input value={price} className="price-slider" onChange={priceHandler} type="range" min="0" max="10000" name="filter-price" />
+
+        </>
+
     )
 }
